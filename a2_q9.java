3dd78bc10da68d10a0203856adbf3a1ae3cfc627
Kashish Rastogi <142674246+kashishrastogi1@users.noreply.github.com>
2024-01-03 15:53:41 +0530

    Create a2_q9.java

---
+++ a2_q9.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Hello
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Enter a no.");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int  i =1;i<=n;i++)
+        {
+            for(int j = 1;j<=n;j++)
+            {
+                if (i == j || i + j == n + 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+            
+        }
+        sc.close();
+    }
+}
